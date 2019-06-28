2e86cd7b8b7947e3d58eddf45f079f8906dc7d6e
lellasone <ketchumjake@gmail.com>
2019-06-28 15:01:04 -0700

    Added per-run programing for odrive

--- software/catkin_ws/src/crawler_control/src/speed.py
+++ software/catkin_ws/src/crawler_control/src/speed.py
@@ -12,14 +12,15 @@ import odrive
 INDEX_PAN = 3
 INDEX_SPEED = 4
 
-MAX_SPEED = 50000
+MAX_SPEED = 100000
 
-def listener():
+def init_node():
 	print("Setting up listener")
 	rospy.init_node('throttle',anonymous=True)
 
+
+def listener():
 	rospy.Subscriber("/spacenav/joy", Joy, callback)
-	
 	print("spinning up listener")
 	rospy.spin()
 	print("listener down")
@@ -32,14 +33,26 @@ def callback(msg):
 def set_speed(speed):
 	engine.axis0.controller.vel_setpoint = speed
 
+
+def set_params():
+	engine.axis0.motor.config.current_lim = 60
+	engine.axis0.controller.config.vel_limit = 120000
+	engine.axis0.motor.config.calibration_current = 20
+	engine.config.brake_resistance = 0.8 
+	engine.axis0.motor.config.pole_pairs = 2
+	engine.axis0.encoder.config.cpr = 2880
+	engine.save_configuration()
+
 def setup_odrive():
 	global engine 
 	print("finding odrive")
 	engine = odrive.find_any()
 	print("odrive found")
 	print(engine.vbus_voltage)
+	set_params()
 
 if __name__ == '__main__':
+	init_node()
 	setup_odrive()
 	listener()
 

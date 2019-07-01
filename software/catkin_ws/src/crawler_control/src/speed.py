bb0d9975751b4743b575248f850a31628fbf1fc1
lellasone <ketchumjake@gmail.com>
2019-07-01 10:21:19 -0700

    Fixed current limiting error with odrive.

--- software/catkin_ws/src/crawler_control/src/speed.py
+++ software/catkin_ws/src/crawler_control/src/speed.py
@@ -9,10 +9,11 @@ import rospy
 from sensor_msgs.msg import Joy
 import odrive
 
-INDEX_PAN = 3
-INDEX_SPEED = 4
+INDEX_PAN = 0
+INDEX_SPEED = 1
 
-MAX_SPEED = 100000
+MAX_SPEED = 200000 #in pulses per revolution (hardware dependent)
+MAX_CURRENT = 80 #in amps
 
 def init_node():
 	print("Setting up listener")
@@ -29,19 +30,23 @@ def callback(msg):
 	speed = msg.axes[INDEX_SPEED]
 	speed = speed * -MAX_SPEED #scale from to correct speeds.
 	set_speed(int(speed))
+	rospy.loginfo("speed_requested: " +str(speed))
+	rospy.loginfo("Iq_measured: " + str(engine.axis0.motor.current_control.Iq_measured))
 
 def set_speed(speed):
+	#TODO: add try-except statment. 
 	engine.axis0.controller.vel_setpoint = speed
 
 
 def set_params():
-	engine.axis0.motor.config.current_lim = 60
-	engine.axis0.controller.config.vel_limit = 120000
+	engine.axis0.motor.config.current_lim = MAX_CURRENT # Set maximum allowable current. 
+	engine.axis0.controller.config.vel_limit = MAX_SPEED + 50000 #add some buffer to prevent errors. 
 	engine.axis0.motor.config.calibration_current = 20
 	engine.config.brake_resistance = 0.8 
 	engine.axis0.motor.config.pole_pairs = 2
 	engine.axis0.encoder.config.cpr = 2880
-	engine.save_configuration()
+	engine.axis0.motor.config.requested_current_range = MAX_CURRENT #set current sense gains. 
+	#engine.save_configuration() #Not needed if called on startup. 
 
 def setup_odrive():
 	global engine 
